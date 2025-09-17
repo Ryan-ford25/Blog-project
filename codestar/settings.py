6fd279b22f5422b1b5cac0024ce4fd8acfe13a7e
Ryan Ford <fordryan01@hotmail.co.uk>
2025-09-17 19:15:40 +0100

    Adding a secret key to the environment and heroku app for security

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zzbxdve4o))osb01vq3k1=6ylp^=_$o=^5a5ukth8%@!4ibw*o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- env.py
+++ env.py
@@ -3,3 +3,7 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_PJTKuy87xqHM@ep-odd-bar-ag06oh93.c-2.eu-central-1.aws.neon.tech/last_skit_rise_971940"
 )
+
+os.environ.setdefault(
+    "SECRET_KEY", "X0`J8|S1Ht'%n#yHan/F"
+)
